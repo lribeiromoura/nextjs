22e996ea5a1d020524d20afd92a1fd10ca08eacc
Leandro Ribeiro Moura <leandro.moura@N0137347.local>
2023-05-11 14:43:45 -0300

    First commit

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Link from '../src/components/Link';
+
+export default function Page404() {
+    return (
+        <div>
+            <h1>Página 404</h1>
+            <Link href="/"> Home </Link>
+        </div>
+    )
+}

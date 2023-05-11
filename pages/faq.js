20b0ff09f9b916f127c898553412f7d858dc0c6d
Leandro Ribeiro Moura <leandro.moura@N0137347.local>
2023-05-11 14:52:02 -0300

    feature: add faq pages

--- pages/faq.js
+++ pages/faq.js
@@ -1,37 +1,23 @@
-import Link from '../src/components/Link';
+import FAQScreen from '../src/screens/FAQScreen';
 
-export async function getServerSideProps() {
+export default FAQScreen; 
+
+export async function getStaticProps() {
+    console.log('Em modo DEV, sempre roda! A cada acesso')
+    console.log('Roda SOMENTE em build time')
     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
     const faq = await fetch(FAQ_API_URL)
-            .then((res) => {
-                return res.json()
-            })
-            .then((resposta) => {
-                return resposta;
-            })
-    return {
-        props: {faq}
-    };
-}
+        .then((respostaDoServidor) => {
+            return respostaDoServidor.json();
+        })
+        .then((resposta) => {
+            return resposta;
+        });
 
-export default function FAQPage(props) {
-    return (
-        <div>
-            <h1>Alura Cases - FAQ</h1>
-            <Link href="/"> home </Link>
-            <ul>
-                {
-                    props.faq.map(({answer, question}) => (
-                        <li key={question}>
-                            <article>
-                                <h2> { question } </h2>
-                                <p> { question } </p>
-                            </article>
-                        </li>
-                    ))
-                }
-                
-            </ul>
-        </div>
-    )
-}
+    return {
+      props: {
+          qualquercoisa: 'que eu passar aqui',
+          faq,
+      },
+    };
+}  
